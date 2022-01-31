1158a7d2eb01234aaeeac84fc3c1656b93d7e831
Charlotte Liang <chliang@google.com>
2022-01-31 14:05:04 -0800

    fix test breakage

--- GoogleUtilitiesMulticastAppDelegate/Tests/Unit/GULMulticastAppDelegateTest.m
+++ GoogleUtilitiesMulticastAppDelegate/Tests/Unit/GULMulticastAppDelegateTest.m
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import "GoogleMulticastAppDelegate/Sources/Public/GoogleUtilities/GULMulticastAppDelegate.h"
+#import "GoogleUtilitiesMulticastAppDelegate/Sources/Public/GoogleUtilities/GULMulticastAppDelegate.h"
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
