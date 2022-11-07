1223da5cc8ada5901393390363d78edd374e8ed7
samedson <samedson@google.com>
2022-11-07 17:15:50 -0500

    Fix watchOS builds from CoreTelephony include

--- GoogleUtilities/Environment/NetworkInfo/GULNetworkInfo.m
+++ GoogleUtilities/Environment/NetworkInfo/GULNetworkInfo.m
@@ -16,12 +16,11 @@
 
 #import <Foundation/Foundation.h>
 
-#import <SystemConfiguration/SystemConfiguration.h>
-
 #import <TargetConditionals.h>
 #if __has_include("CoreTelephony/CTTelephonyNetworkInfo.h") && !TARGET_OS_MACCATALYST && \
-                  !TARGET_OS_OSX && !TARGET_OS_TV
+                  !TARGET_OS_OSX && !TARGET_OS_TV && !TARGET_OS_WATCH
 #define TARGET_HAS_MOBILE_CONNECTIVITY
+#import <SystemConfiguration/SystemConfiguration.h>
 #import <CoreTelephony/CTCarrier.h>
 #import <CoreTelephony/CTTelephonyNetworkInfo.h>
 #endif
