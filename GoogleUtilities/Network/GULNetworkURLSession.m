1913ab817e7f01d1339bda9ec40c34bfd35af37e
Paul Beusterien <paulbeusterien@google.com>
2024-05-14 12:49:24 -0700

    Fix analyzer error (#158)

--- GoogleUtilities/Network/GULNetworkURLSession.m
+++ GoogleUtilities/Network/GULNetworkURLSession.m
@@ -145,25 +145,25 @@
     [self excludeFromBackupForURL:_uploadingFileURL];
 
     _sessionConfig = [self backgroundSessionConfigWithSessionID:_sessionID];
-    [self populateSessionConfig:_sessionConfig withRequest:request];
-    session = [NSURLSession sessionWithConfiguration:_sessionConfig
-                                            delegate:self
-                                       delegateQueue:[NSOperationQueue mainQueue]];
-    postRequestTask = [session uploadTaskWithRequest:request fromFile:_uploadingFileURL];
   } else {
     // If we cannot write to file, just send it in the foreground.
     _sessionConfig = [NSURLSessionConfiguration defaultSessionConfiguration];
-    [self populateSessionConfig:_sessionConfig withRequest:request];
-    session = [NSURLSession sessionWithConfiguration:_sessionConfig
-                                            delegate:self
-                                       delegateQueue:[NSOperationQueue mainQueue]];
-    // To avoid a runtime warning in Xcode 15 Beta 4, the given `URLRequest`
-    // should have a nil `HTTPBody`. To workaround this, the given `URLRequest`
-    // is copied and the `HTTPBody` data is removed.
-    NSData *givenRequestHTTPBody = [request.HTTPBody copy];
-    NSMutableURLRequest *requestWithoutHTTPBody = [request mutableCopy];
-    requestWithoutHTTPBody.HTTPBody = nil;
+  }
+  [self populateSessionConfig:_sessionConfig withRequest:request];
+  session = [NSURLSession sessionWithConfiguration:_sessionConfig
+                                          delegate:self
+                                     delegateQueue:[NSOperationQueue mainQueue]];
+  // To avoid a runtime warning in Xcode 15 Beta 4, the given `URLRequest`
+  // should have a nil `HTTPBody`. To workaround this, the given `URLRequest`
+  // is copied and the `HTTPBody` data is removed.
+  NSData *givenRequestHTTPBody = [request.HTTPBody copy];
+  NSMutableURLRequest *requestWithoutHTTPBody = [request mutableCopy];
+  requestWithoutHTTPBody.HTTPBody = nil;
 
+  if (didWriteFile) {
+    postRequestTask = [session uploadTaskWithRequest:requestWithoutHTTPBody
+                                            fromFile:_uploadingFileURL];
+  } else {
     postRequestTask = [session uploadTaskWithRequest:requestWithoutHTTPBody
                                             fromData:givenRequestHTTPBody];
   }
