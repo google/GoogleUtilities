12ee963dbdbbc13be637afdfa6e5451163294202
Andrew Heard <andrewheard@google.com>
2024-08-08 17:02:48 -0400

    Force log level error

--- GoogleUtilities/Logger/GULLogger.m
+++ GoogleUtilities/Logger/GULLogger.m
@@ -72,7 +72,7 @@ GULLoggerLevel GULGetLoggerLevel(void) {
 
 __attribute__((no_sanitize("thread"))) void GULSetLoggerLevel(GULLoggerLevel loggerLevel) {
   if (loggerLevel < GULLoggerLevelMin || loggerLevel > GULLoggerLevelMax) {
-    GULOSLogError(kGULLogSubsystem, kGULLoggerLogger, NO, @"I-COR000023",
+    GULOSLogError(kGULLogSubsystem, kGULLoggerLogger, YES, @"I-COR000023",
                   @"Invalid logger level, %ld", (long)loggerLevel);
     return;
   }
